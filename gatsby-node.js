8651facb979810fd7e0e2ef5a2efd2a5e57bf196
wille <w.sandstrom@gmail.com>
2018-11-02 14:24:48 +0100

    stores pages in output

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,23 @@
 const fs = require('fs');
 
-exports.onPostBuild = async ({ store }, pluginOptions) => {
-  const { redirects } = store.getState();
-  await fs.writeFile(
-    pluginOptions.output || 'redirects.json',
-    JSON.stringify(redirects, null, 2)
+exports.onPostBuild = ({ store }, pluginOptions) => {
+  const { redirects, pages } = store.getState();
+
+  const p = [];
+  for (const page of pages.values()) {
+    p.push({
+      path: page.path,
+      matchPath: page.matchPath,
+    });
+  }
+
+  const data = {
+    redirects,
+    pages: p,
+  };
+
+  fs.writeFileSync(
+    pluginOptions.output || 'gatsby-express.json',
+    JSON.stringify(data, null, 2)
   );
 };
--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const fs = require('fs');
 
-module.exports = function redirect(data = 'redirects.json') {
+module.exports = function redirect(data = 'gatsby-express.json') {
   if (typeof data === 'string') {
     data = fs.readFileSync(data);
     data = JSON.parse(data);
   }
 
   return function(req, res, next) {
-    for (var r of data) {
+    for (var r of data.redirects) {
       if (req.path === r.fromPath) {
         const code = r.isPermanent ? 301 : 302;
         return res.redirect(code, r.toPath);
+      }
+    }
+
+    for (var page of data.pages) {
+      if (req.path === page.path) {
+        return res.sendStatus(404);
       }
     }
 
