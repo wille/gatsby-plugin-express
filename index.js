1b500b4fb49e6d4c8df1f43ab5081e9495b6fe96
wille <w.sandstrom@gmail.com>
2018-11-02 15:14:37 +0100

    supports client only routes

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
+const path = require('path');
+const match = require('@reach/router/lib/utils').match;
 
-module.exports = function redirect(data = 'gatsby-express.json') {
+module.exports = function redirect(data = 'gatsby-express.json', root) {
   if (typeof data === 'string') {
     data = fs.readFileSync(data);
     data = JSON.parse(data);
   }
 
-  return function(req, res, next) {
+  return async function(req, res, next) {
     for (var r of data.redirects) {
       if (req.path === r.fromPath) {
         const code = r.isPermanent ? 301 : 302;
@@ -14,12 +16,24 @@ module.exports = function redirect(data = 'gatsby-express.json') {
       }
     }
 
-    for (var page of data.pages) {
+    for (var page of data.pages) {
+      const b = page.matchPath && match(page.matchPath, req.path);
+      if (b) {
+        const html = require.resolve('index.html', {
+          paths: [
+            path.join(root, b.uri.slice(1))
+          ]
+        });
+
+        return res.sendFile(html);
+      }
+
+      // strict match
       if (req.path === page.path) {
-        return res.sendStatus(404);
+        return next();
       }
     }
 
-    next();
+    return res.sendStatus(404);
   }
 }
